712da50f7266ad4a47c4f97d329b8fa494a75386
unknown <thiago_b_alves@outlook.com>
2023-08-07 20:00:59 -0300

    titularizando nomes

--- modelo.py
+++ modelo.py
@@ -1,19 +1,30 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes +=1
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
 
-vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
+    def dar_like(self):
+        self.likes += 1
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duração: {vingadores.duracao}')
+      f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
-atlanta = Serie('Atlanta',2018, 2)
+atlanta = Serie('atlanta',2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporada: {atlanta.temporadas}')
+      f'- Temporada: {atlanta.temporadas} - Likes: {atlanta.likes}')
