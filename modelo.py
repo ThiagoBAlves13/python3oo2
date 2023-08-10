494d20e9649f36e9c7b146fede3e8cf477c9f972
unknown <thiago_b_alves@outlook.com>
2023-08-10 18:09:03 -0300

    Aula03

--- modelo.py
+++ modelo.py
@@ -36,10 +36,18 @@ class Serie(Programa):
     def __str__(self):
         return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome.title()
-        super().__init__(programas)
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta',2018, 2)
